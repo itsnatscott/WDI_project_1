5cec8e5ba142a484bbf975402f777be80e395b92
Your Name <you@example.com>
2015-04-15 13:07:58 -0400

    got basic index and show page running

--- server.js
+++ server.js
@@ -11,7 +11,40 @@ app.use(bodyParser.urlencoded({
 }));
 var methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+//redirect "/"
+app.get("/", function(req,res){
+	res.redirect("/designistforum");
+});
 
+//show all posts
+app.get('/designistforum', function(req, res){
+	db.all("SELECT * FROM post", function(err, data){
+		if (err) {
+			console.log(err);
+		} else {
+			var posts = data;
+		}
+		res.render("index.ejs",{
+			post: posts
+		});
+	});
+});
+//show individual post
+app.get("/designistforum/:id", function(req,res){
+	var id = req.params.id
+	db.get("SELECT * FROM post WHERE id = ?"
+		, id, function(err,data){
+			item = data
+			res.render('show.ejs', {
+				thisPost: item
+				console.log(thisPost.created_at)
+				});
+		});
+});
+//edit post
+app.put("/designistforum/:id", function(req,res){
+	db.run("UPDATE post SET title = ? , body = ? category)")
+})
 
 
 
